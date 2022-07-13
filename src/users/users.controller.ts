6c3bca2382d370f4959ee227de5ab7446d603d47
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-13 23:29:48 +0300

    feat: fix users's service and controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,10 +10,17 @@ import {
   Inject,
   UseInterceptors,
   ClassSerializerInterceptor,
+  HttpStatus,
+  BadRequestException,
+  NotFoundException,
+  ForbiddenException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDTO } from './dto/update-password.dto';
+import { isValidID } from 'src/utils/isValidID';
+import { ERRORS_MSGS } from 'src/utils/errorsMsg.enum';
+import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UsersController {
@@ -30,13 +37,18 @@ export class UsersController {
   @UseInterceptors(ClassSerializerInterceptor)
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+    if (!isValidID(id)) throw new BadRequestException(ERRORS_MSGS.INVALID_ID);
+    const user = this.usersService.findOne(id);
+    if (user) return user;
+    else throw new NotFoundException(ERRORS_MSGS.USER_NOT_FOUND);
   }
 
   @UseInterceptors(ClassSerializerInterceptor)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+    const user = this.usersService.create(createUserDto);
+    if (user) return user;
+    else throw new BadRequestException(ERRORS_MSGS.NO_REQUIRED_FIELDS);
   }
 
   @UseInterceptors(ClassSerializerInterceptor)
@@ -45,12 +57,21 @@ export class UsersController {
     @Param('id') id: string,
     @Body() updatePasswordDTO: UpdatePasswordDTO,
   ) {
-    return this.usersService.update(id, updatePasswordDTO);
+    if (!isValidID(id)) throw new BadRequestException(ERRORS_MSGS.INVALID_ID);
+    const data = this.usersService.update(id, updatePasswordDTO);
+    if (data instanceof User) return data;
+    else if (data === ERRORS_MSGS.INVALID_PASSWORD)
+      throw new ForbiddenException(ERRORS_MSGS.INVALID_PASSWORD);
+    else throw new NotFoundException(ERRORS_MSGS.USER_NOT_FOUND);
   }
 
   @Delete(':id')
-  @HttpCode(204)
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
-    return this.usersService.remove(id);
+    if (!isValidID(id)) throw new BadRequestException(ERRORS_MSGS.INVALID_ID);
+    const userIndex = this.usersService.remove(id);
+    if (userIndex === -1)
+      throw new NotFoundException(ERRORS_MSGS.USER_NOT_FOUND);
+    else return;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { isValidID } from 'src/utils/isValidID';
+import { Injectable } from '@nestjs/common';
+import { ERRORS_MSGS } from 'src/utils/errorsMsg.enum';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDTO } from './dto/update-password.dto';
@@ -14,11 +14,8 @@ export class UsersService {
   }
 
   findOne(id: string) {
-    if (!isValidID(id))
-      throw new HttpException('ID Is Not Valid', HttpStatus.BAD_REQUEST);
-    const user: User = this.users.find((user) => user.id === id);
-    if (user) return user;
-    else throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
+    const user = this.users.find((user) => user.id === id);
+    return user;
   }
 
   create(createUserDto: CreateUserDto) {
@@ -30,39 +27,25 @@ export class UsersService {
       updatedAt: Date.now(),
       version: 1,
     });
-    if (user instanceof User) {
-      this.users.push(user);
-      return user;
-    } else
-      throw new HttpException(
-        'User Not containing required fields',
-        HttpStatus.BAD_REQUEST,
-      );
+    this.users.push(user);
+    return user;
   }
 
   update(id: string, updatePasswordDTO: UpdatePasswordDTO) {
-    if (!isValidID(id))
-      throw new HttpException('ID Is Not Valid', HttpStatus.BAD_REQUEST);
     const user = this.users.find((user) => user.id === id);
     if (user && user.password === updatePasswordDTO.oldPassword) {
       user.password = updatePasswordDTO.newPassword;
       user.updatedAt = Date.now();
       user.version++;
-      console.log(user);
-      return user;
     } else if (user && user.password !== updatePasswordDTO.oldPassword) {
-      throw new HttpException('Password Not Valid', HttpStatus.FORBIDDEN);
-    } else {
-      throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
+      return ERRORS_MSGS.INVALID_PASSWORD;
     }
+    return user;
   }
 
   remove(id: string) {
-    if (!isValidID(id))
-      throw new HttpException('ID Is Not Valid', HttpStatus.BAD_REQUEST);
     const userIndex = this.users.findIndex((user) => user.id === id);
-    if (userIndex === -1)
-      throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
     this.users.splice(userIndex, 1);
+    return userIndex;
   }
 }
