aa2f9c2f70c7c5cfafa5c3a0ea7fb345ff8f69f8
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-13 22:48:20 +0300

    init project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { cwd } from 'process';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const DOCS_FILE = join(cwd(), 'doc', 'api.yaml');
+  const DOCS_DATA = await readFile(DOCS_FILE, 'utf-8');
+  const document = parse(DOCS_DATA);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(4000);
 }
 bootstrap();
