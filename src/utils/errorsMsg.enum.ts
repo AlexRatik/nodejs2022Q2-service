c2272eea51e79bac2dcd97e9b345bf26bef29452
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-15 12:52:25 +0300

    fix errorsMsg enum

--- src/utils/errorsMsg.enum.ts
+++ src/utils/errorsMsg.enum.ts
@@ -2,6 +2,7 @@ export enum ERRORS_MSGS {
   USER_NOT_FOUND = 'User not found',
   TRACK_NOT_FOUND = 'Track not found',
   ARTIST_NOT_FOUND = 'Artist not found',
+  ALBUM_NOT_FOUND = 'Album not found',
   INVALID_ID = 'ID is invalid',
   NO_REQUIRED_FIELDS = 'No required fields',
   INVALID_PASSWORD = 'Password is invalid',
